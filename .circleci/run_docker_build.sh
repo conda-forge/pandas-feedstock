5d8597f693e9e04d5f14c09915c556d04024edb7
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-05-01 13:42:25 -0500

    MNT: Re-rendered with conda-smithy 3.1.2 and pinning 2018.04.28

--- .circleci/run_docker_build.sh
+++ .circleci/run_docker_build.sh
@@ -60,7 +60,7 @@ conda install --yes --quiet conda-forge-ci-setup=1 conda-build
 source run_conda_forge_build_setup
 
 conda build /home/conda/recipe_root -m /home/conda/feedstock_root/.ci_support/${CONFIG}.yaml --quiet || exit 1
-upload_or_check_non_existence /home/conda/recipe_root conda-forge --channel=main -m /home/conda/feedstock_root/.ci_support/${CONFIG}.yaml || exit 1
+upload_or_check_non_existence /home/conda/recipe_root conda-forge --channel=rc -m /home/conda/feedstock_root/.ci_support/${CONFIG}.yaml || exit 1
 
 touch /home/conda/feedstock_root/build_artifacts/conda-forge-build-done
 EOF
